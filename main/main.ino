ab8085d68588b3be48d1caf2b6ef1ff841909d62
Avery Vine <averyvine@icloud.com>
2018-01-27 13:17:46 -0500

    added basic hardware recognition code

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,22 @@
+const int buttonPin = 2;
+const int accelPin = 7;
+int buttonState = 0;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(buttonPin, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  Serial.print("Button and LED prepared.");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  buttonState = digitalRead(buttonPin);
+  Serial.print(buttonState);
+  if (buttonState == HIGH) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 }
