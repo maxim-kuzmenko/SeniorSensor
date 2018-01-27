aa5e292cc6e5e390526a8efa538abb66bef81624
Kirk Yuan <yuanjt810@gmail.com>
2018-01-27 16:58:45 -0500

    starting to parse json into javascript object

--- app.js
+++ app.js
@@ -6,5 +6,18 @@ port.on('open', function(){
   console.log('Serial Port Opened');
   port.on('data', function(data){
       console.log(data.toString());
+      customhandle(data.toString());
   });
 });
+
+function customhandle(data){
+	console.log(data);
+	// turn string to json then javascript object
+	var splitstring = data.split('/\r?\n|\r/');
+	console.log(splitstring);
+	// read individual variables
+	var output = JSON.stringify(splitstring);
+	// change positions to Tilt-sensing
+	
+	// alert text if position changes
+} 
