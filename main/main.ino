61be71bf307a2d42e829a465c15b59098fbe9006
Kirk Yuan <yuanjt810@gmail.com>
2018-01-27 13:21:24 -0500

    Added accel code

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,6 @@
+#include <Wire.h>
+#include "MMA7660.h"
+MMA7660 accelemeter;
 const int buttonPin = 2;
 const int accelPin = 7;
 int buttonState = 0;
@@ -6,8 +9,10 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(buttonPin, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
+  accelemeter.init();
   Serial.begin(9600);
   Serial.print("Button and LED prepared.");
+  // Accelpin
 }
 
 void loop() {
@@ -19,4 +24,16 @@ void loop() {
   } else {
     digitalWrite(LED_BUILTIN, LOW);
   }
+  int8_t x;
+  int8_t y;
+  int8_t z;
+  float ax,ay,az;
+  accelemeter.getXYZ(&x,&y,&z);
+  
+  Serial.print("x-coordinate : ");
+    Serial.println(x); 
+    Serial.print("y-coord : ");
+    Serial.println(y);   
+    Serial.print("z-coord : ");
+    Serial.println(z);
 }
